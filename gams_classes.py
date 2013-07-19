86e1b4fac46df8996d0b9d634479227bb46acb04
Joel Goop <joel.goop@chalmers.se>
2013-07-19 16:47:57 +0200

    New structure for adding sets and parameters to a model and functions to write them to files.

--- gams_classes.py
+++ gams_classes.py
@@ -1,15 +1,38 @@
 import os
 import subprocess as sp
 from PySide import QtCore
+from ..misc.helper_classes import Struct
 
 class GamsModel(object):
     """Class that contains necessary data to run a GAMS model"""
-    def __init__(self, gams_dir, model_file, res_file):
+    def __init__(self, gams_dir, model_file, res_file, elem_collections=[]):
         super(GamsModel, self).__init__()
         self.gams_dir = os.path.abspath(gams_dir)
         os.chdir(self.gams_dir)
         self.model_file = model_file
         self.res_file = res_file
+        self.elem_collections = elem_collections
+
+    def add_element_collection(self,elem_collection):
+        self.elem_collections.append(elem_collection)
+
+    # Write all elements to their respective files
+    def write_elements(self):
+        for c in self.elem_collections:
+            c.write_elements()
+
+    # Returns results dictionary
+    def get_results(self):
+        return self._results
+
+    # Read results from the put file generated from GAMS
+    # Place in a dictionary {(set_key1,...): value}
+    def read_results_as_tuple_dict(self):
+        with open(self.model.res_file,'r') as f:
+            self._results = {tuple(parts[:-1]): float(parts[-1]) for parts in (line.split() for line in f)}
+
+    def full_file_name(self,f_name):
+        return self.gams_dir + '/' + f_name
 
     # Property containing the model file name
     def model_file():
@@ -42,6 +65,7 @@ class GamsModel(object):
     res_file = property(**res_file())
 
 
+
 class GamsThread(QtCore.QThread):
     """Class to run GAMS in a separate QThread"""
 
@@ -63,11 +87,8 @@ class GamsThread(QtCore.QThread):
     # Read results from the put file generated from GAMS
     # Place in a dictionary {(set_key1,...): value}
     def read_results_as_tuple_dict(self):
-        self._results = {}
         with open(self.model.res_file,'r') as f:
-            for line in f:
-                parts = line.split()
-                self._results[tuple(parts[:-1])] = float(parts[-1])
+            self._results = {tuple(parts[:-1]): float(parts[-1]) for parts in (line.split() for line in f)}
 
     # Read results from the put file generated from GAMS
     def read_results(self):
@@ -92,26 +113,27 @@ class GamsThread(QtCore.QThread):
 
 class GamsElement(object):
     """Class to handle an element such as a set or parameter"""
-    def __init__(self, name, data):
+    def __init__(self, name, data, identifier):
         super(GamsElement, self).__init__()
         self.name = name
         self.data = data
+        self.identifier = identifier
         self.data_start = '/'
-        self.data_end = '/;'
+        self.data_end = '/;\n'
 
-    def write_to_inc(self,inc_file,include_head_foot=True):
-        with open(inc_file,'wb') as f:
-            # Write header
-            if include_head_foot:
-                f.write('{name}{keys} {data_start}\n'
-                            .format(name=self.name,
-                                    keys=self._get_keys_as_string(),
-                                    data_start=self.data_start))
-            # Write data
-            f.write('{data}\n'.format(data='\n'.join(self._get_data_as_strings())))
-            # Write footer
-            if include_head_foot:
-                f.write('{data_end}'.format(data_end=self.data_end))
+    def write_to_inc(self,f,include_head_foot=True):
+        # Write header
+        if include_head_foot:
+            f.write('{identifier} {name}{keys} {data_start}\n'
+                        .format(identifier=self.identifier,
+                                name=self.name,
+                                keys=self._get_keys_as_string(),
+                                data_start=self.data_start))
+        # Write data
+        f.write('{data}\n'.format(data='\n'.join(self._get_data_as_strings())))
+        # Write footer
+        if include_head_foot:
+            f.write('{data_end}'.format(data_end=self.data_end))
 
     def _get_keys_as_string(self):
         raise NotImplementedError("Abstract class. Use subclasses!")
@@ -130,10 +152,11 @@ class GamsElement(object):
         return locals()
     data = property(**data())
 
+
 class GamsSet(GamsElement):
     """Class to represent a set in GAMS"""
     def __init__(self, name, elements):
-        super(GamsSet, self).__init__(name,elements)
+        super(GamsSet, self).__init__(name,elements,'SET')
 
     def _get_keys_as_string(self):
         return ''
@@ -141,19 +164,39 @@ class GamsSet(GamsElement):
     def _get_data_as_strings(self):
         return self.data
 
+
 class GamsParameter(GamsElement):
     """Class to represent a parameter in GAMS"""
     def __init__(self, name, data, set_names=[]):
-        super(GamsParameter, self).__init__(name,data)
+        super(GamsParameter, self).__init__(name,data,'PARAMETER')
         self.set_names = set_names
+        if not set_names:
+            self._get_data_as_strings = self._get_data_as_strings_1d
+        else:
+            self._get_data_as_strings = self._get_data_as_strings_nd
 
     def _get_keys_as_string(self):
         return '' if not self.set_names else '('+','.join(self.set_names)+')'
 
-    def _get_data_as_strings(self):
+    def _get_data_as_strings_nd(self):
         return ['.'.join(key)+'\t\t'+str(val) for key,val in self.data.items()]
 
+    def _get_data_as_strings_1d(self):
+        return [str(self.data)]
 
 
+class GamsElementCollection(object):
+    """Handle collections of elements"""
+    def __init__(self, elem_file, elements=[]):
+        super(GamsElementCollection, self).__init__()
+        self.elements = elements
+        self.elem_file = elem_file
 
+    def add_element(self,element):
+        self.elements.append(element)
 
+    # Write all elements to file
+    def write_elements(self):
+        with open(self.elem_file,'wb') as f:
+            for el in self.elements:
+                el.write_to_inc(f)
