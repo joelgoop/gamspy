956ac8135d1eabdf6403431e86cc3ad7cb8a665a
Joel Goop <joel.goop@chalmers.se>
2014-10-07 17:53:08 +0200

    Changed to include gamspy directly and not mess with path

--- gamspy/tests/test_package_gamspy.py
+++ gamspy/tests/test_package_gamspy.py
@@ -1,5 +1,4 @@
 import sys, os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import pytest
 from gamspy.examples import TransportModel
 from gamspy.utils import make_tmp_dir
